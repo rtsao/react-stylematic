78fa6e933469f5ecafd4bfd7863dc23930a9fb6c
Ryan Tsao <rtsao@uber.com>
2016-06-06 17:46:19 -0700

    prefix fallback values beta

--- test/index.js
+++ test/index.js
@@ -31,7 +31,9 @@ test('style prop with passthrough and children', function(t) {
   styletron.startBuffering();
   t.deepEqual(
     createElementStylematic('div', {style: {color: ['rgba(0,0,255,0.5)', 'blue']}}, createElement('div')),
-    createElement('div', {className: '_style_3Bl7iO'}, createElement('div'))
+    createElement('div', {className: '_style_4fAcOm'}, createElement('div'))
   );
+  var css = styletron.flushBuffer();
+  t.equal(css, '._style_4fAcOm {\n  color: rgba(0,0,255,0.5) !important;\n  color: blue !important\n}');
   t.end();
 });
